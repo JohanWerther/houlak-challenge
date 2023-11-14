32f54ea62d76a15d62dd04824538eed7790d42fc
JohanWerther <fedelabandeira490@gmail.com>
2023-11-14 18:56:03 -0300

    add libraries context providers
    Theme from shadcn and Cookies from react-cookie

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { ThemeProvider } from "./components/theme-provider.tsx";
+import { CookiesProvider } from "react-cookie";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider>
+      <CookiesProvider>
+        <App />
+      </CookiesProvider>
+    </ThemeProvider>
+  </React.StrictMode>
+);
