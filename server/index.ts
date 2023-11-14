8d8c0559afd31d092ec60e358f38e04415454684
JohanWerther <fedelabandeira490@gmail.com>
2023-11-14 19:10:05 -0300

    add auth, api router, and error middleware on sv

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,30 @@
-import express from "express";
+import "dotenv/config";
 import ViteExpress from "vite-express";
+import cookieParser from "cookie-parser";
+import express, { NextFunction, Request, Response } from "express";
+
+import { AppError } from "./models/app-error.ts";
+import { apiRouter } from "./router/api/index.ts";
+import { authRouter } from "./router/auth/index.ts";
 
 const app = express();
+app.use(cookieParser());
 
-app.get("/message", (_, res) => res.send("Hello from express!"));
+app.use("/api", apiRouter);
+app.use("/auth", authRouter);
+
+app.use(
+  <T extends Error>(
+    err: T,
+    _req: Request,
+    res: Response,
+    _next: NextFunction
+  ) => {
+    if (err instanceof AppError) {
+      return res.status(err.statusCode).json({ message: err.message });
+    }
+    res.status(500).json({ message: "Something went wrong" });
+  }
+);
 
 ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
