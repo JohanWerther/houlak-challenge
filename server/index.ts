6971a22d3c77bbb4fc63ebaee559d9224e90ee75
JohanWerther <fedelabandeira490@gmail.com>
2023-11-15 19:09:19 -0300

    fix server shutdown
    I was having an issue with the port address as it was already in use because the server wasn't closing gracefully

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,15 @@
 import "dotenv/config";
+import express from "express";
 import ViteExpress from "vite-express";
 import cookieParser from "cookie-parser";
-import express, { NextFunction, Request, Response } from "express";
 
+import type http from "http";
+import type { NextFunction, Request, Response } from "express";
+
+import { AxiosError } from "axios";
 import { AppError } from "./models/app-error.ts";
 import { apiRouter } from "./router/api/index.ts";
 import { authRouter } from "./router/auth/index.ts";
-import { AxiosError } from "axios";
 import { sequelize } from "./database/db.ts";
 
 const app = express();
@@ -39,16 +42,30 @@ app.use(
   }
 );
 
+let server: http.Server;
+
 sequelize
   .sync()
   .then(() => {
     console.log("Database sincronized");
-    ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
+    server = ViteExpress.listen(app, 3000, () =>
+      console.log("Server is listening...")
+    );
   })
   .catch(() => {
     console.log("Could not init Database");
+    process.exit(1);
   });
 
-process.on("SIGINT", () => {
+function handleShutdown() {
   sequelize.close().catch(console.log);
-});
+  if (server) {
+    server.close(() => {
+      console.log("Server closed. Exiting process.");
+      process.exit(0);
+    });
+  }
+}
+
+process.on("SIGINT", handleShutdown);
+process.on("SIGTERM", handleShutdown);
