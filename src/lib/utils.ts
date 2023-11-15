36bf1b585b99cc97383e503a611c86339a2f5c29
JohanWerther <fedelabandeira490@gmail.com>
2023-11-15 13:29:13 -0300

    add http error class on frontend

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export class HttpError extends Error {
+  statusCode: number;
+  constructor(message: string, status: number) {
+    super(message);
+    this.statusCode = status;
+  }
 }
