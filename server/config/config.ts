5515a4002d7b434f9329019c8c76b519893b3c0f
JohanWerther <fedelabandeira490@gmail.com>
2023-11-17 10:24:29 -0300

    remove unused vars

--- server/config/config.ts
+++ server/config/config.ts
@@ -1,8 +1,3 @@
-// cookies keys
-export const ACCESS_TOKEN_COOKIE = "hc-spotify-access-token";
-export const REFRESH_TOKEN_COOKIE = "hc-spotify-refresh-token";
-export const AUTH_STATE_KEY = "spotify_auth_state";
-
 // spotify api config
 export const SPOTIFY_CLIENT_ID =
   process.env?.SPOTIFY_CLIENT_ID || "bdda44d2b2714e07bd5506d43794e561";
